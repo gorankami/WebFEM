f13798533f7fe967a64fd9906c8d74ad69afa0da
goran <gorankami@gmail.com>
2019-02-03 08:58:36 +0100

    Addressed npm audit problems. Moved drawLegend function into own file

--- src/directives/legend-view/index.js
+++ src/directives/legend-view/index.js
@@ -1,5 +1,6 @@
 import angular from "angular";
 import $ from "jquery";
+import drawLegend from "../../drawLegend";
 
 /**
  * @desc
@@ -31,51 +32,5 @@ function linkFunc(scope, element) {
 /* @ngInject */
 function LegendViewController($scope) {
   const vm = this;
-  $scope.$on('legend:draw', drawEvent);
-
-  function drawEvent(event, colorMap) {
-
-    const startX = 25,
-      startY = 25,
-      width = 50,
-      height = 450,
-      stepsLength = colorMap.steps.length,
-      gradient = $scope.inverted ?
-      vm.context.createLinearGradient(0, startY, 0, startY + height) :
-      vm.context.createLinearGradient(0, startY + height, 0, startY),
-      colorStopPercentage = 1 / (stepsLength - 1).toFixed(2),
-      tickHeight = (height + 1) / (stepsLength - 1).toFixed(2);
-
-    vm.context.fillStyle = "Black";
-    vm.context.fillRect(0, 0, 150, 500);
-    vm.context.strokeStyle = "White";
-
-    for (let i = 0; i < stepsLength; i++) {
-      gradient.addColorStop(i * colorStopPercentage, colorMap.steps[i].color.getStyle());
-    }
-    vm.context.fillStyle = gradient;
-    vm.context.fillRect(startX, startY, width, height);
-
-    //table border
-    vm.context.beginPath();
-    vm.context.moveTo(startX - 0.5, startY - 0.5);
-    vm.context.lineTo(startX + width + 0.5, startY - 0.5);
-    vm.context.lineTo(startX + width + 0.5, startY + height + 0.5);
-    vm.context.lineTo(startX - 0.5, startY + height + 0.5);
-    vm.context.lineTo(startX - 0.5, startY - 0.5);
-    vm.context.stroke();
-    // ...and ticks
-    vm.context.beginPath();
-    vm.context.font = "10px Verdana";
-    vm.context.fillStyle = "White";
-
-    for (let i = 0; i < stepsLength; i++) {
-      let xpos = startX + width;
-      let ypos = Math.round(startY + tickHeight * (stepsLength - 1 - i)) - 0.5;
-      vm.context.moveTo(xpos - 5.5, ypos);
-      vm.context.lineTo(xpos + 5.5, ypos);
-      vm.context.fillText(colorMap.steps[i].scaledVal ? colorMap.steps[i].scaledVal.toFixed(4) : 0, xpos + 10, ypos + 4);
-    }
-    vm.context.stroke();
-  }
+  $scope.$on('legend:draw', (event, colorMap) => drawLegend(vm.context, colorMap));
 }
